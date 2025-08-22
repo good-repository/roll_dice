b55eb67d2f869b9c674e0c43bd57eb36c10211bd
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2025-08-21 22:01:20 -0300

    Update main.dart with gradient background and styled text

--- lib/main.dart
+++ lib/main.dart
@@ -10,17 +10,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.blue),
       home: Scaffold(
-        appBar: AppBar(title: const Text('First App')),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text('Hello World!'),
-              Text('It\'s time to learn Flutter!'),
-            ],
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.amber, Colors.red],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello World!',
+              style: TextStyle(color: Colors.white, fontSize: 28),
+            ),
           ),
         ),
       ),
